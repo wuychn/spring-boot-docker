12f5d24b61db7b0fa3f0d9358b78b2e25d15d9a3
wuychn <wuychn@163.com>
2019-05-03 17:54:44 +0800

    添加配置文件

--- src/main/java/com/wuychn/DemoApplication.java
+++ src/main/java/com/wuychn/DemoApplication.java
@@ -2,12 +2,14 @@ package com.wuychn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -18,4 +20,8 @@ public class DemoApplication {
         return "hello, Jenkins!";
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 }
